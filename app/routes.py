a1711d01828d1f3997dad72035b210c4d376b734
hima890 <hfibrahim90@gmail.com>
2025-01-19 16:22:01 +0200

    feature add-profile-endpoint

--- app/routes.py
+++ app/routes.py
@@ -101,3 +101,32 @@ def apisettings(username, session):
     
     else:
         return redirect('/login')
+
+
+@main.route('/profile/<string:username>/<string:session>', methods=['GET', 'POST'])
+def profile(username, session):
+    
+    global logged_in
+
+    if username in logged_in and (logged_in[username]['object'].session_id == session):
+        user = {
+            "username" : username,
+            "image":"/static/images/amanSingh.jpg",
+            "api": logged_in[username]["object"].api,
+            "session" : session,
+            "firstname": logged_in[username]["object"].first,
+            "lastname": logged_in[username]["object"].last,
+            "email":logged_in[username]["object"].email,
+            "phone":logged_in[username]["object"].phone,
+            "lastlogin":logged_in[username]["object"].last_login,
+        }
+
+        devices = [
+            {"Dashboard" : "device1",
+            "deviceID": "ARMS12012"
+            }
+        ]
+        return render_template('profile.htm', title='API-Settings', user=user, devices=devices)
+    
+    else:
+        return redirect('/login')
