c3f1162d5648c8b0eaea6a73979a0cdb31367dd2
hima890 <hfibrahim90@gmail.com>
2025-01-19 15:37:56 +0200

    feature setup-project-structure

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv("./.env")
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
+    DEBUG = os.environ.get('SECRET_KEY', 'default_secret_key')
