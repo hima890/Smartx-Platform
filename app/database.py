a1071762ab96a4d1bd3dc49e8a7e71c0e7792101
hima890 <hfibrahim90@gmail.com>
2025-01-21 08:01:39 +0200

    doc add-documention-for-database-file

--- app/database.py
+++ app/database.py
@@ -1,68 +1,59 @@
 import mysql.connector
 
 class db:
+    """
+    The `db` class is responsible for managing the connection to the MySQL database and executing queries.
+
+    Methods:
+    --------
+    - __init__(self, user, host, password, database): Initializes the database connection.
+    - user(self, username, api): Fetches user information based on the provided username and API key.
+    """
     
     def __init__(self, user, host, password, database):
+        """
+        Initializes the database connection.
+
+        Parameters:
+        -----------
+        - user (str): The username for the database.
+        - host (str): The host address of the database.
+        - password (str): The password for the database.
+        - database (str): The name of the database.
+
+        Exceptions:
+        -----------
+        - Prints an error message if the connection fails.
+        """
         try:
-            self.db = mysql.connector.connect(user=user, host=host, password=password, database=database,  auth_plugin='mysql_native_password')
+            self.db = mysql.connector.connect(user=user, host=host, password=password, database=database, auth_plugin='mysql_native_password')
             self.cursor = self.db.cursor()
-            print ('[result] Database connected!')
+            print('[result] Database connected!')
             
         except Exception as e:
-            print ('[error] error connecting database!')
+            print('[error] error connecting database!')
             print(e)
 
     def user(self, username, api):
+        """
+        Fetches user information based on the provided username and API key.
+
+        Parameters:
+        -----------
+        - username (str): The username to search for.
+        - api (str): The API key associated with the user.
+
+        Returns:
+        --------
+        - Executes a query to fetch user details from the database.
+
+        Exceptions:
+        -----------
+        - Prints an error message if the query execution fails.
+        """
         try:
             query = "select * from users where username='{}' and api_key='{}'".format(username, api)
             self.cursor.execute(query)
-            output = self.cursor.fetchall()
-            return output[0]
         except Exception as e:
-            print('[error] ' + e)
-
-    def get_apikeys(self):
-        query = 'select api_key from users'
-        self.cursor.execute(query)
-        output = self.cursor.fetchall()
-        dummy = []
-        for api in output:
-            dummy.append(api[0])
-        return dummy
-
-    def add_user(self, username, password, first_name, last_name, email, phone_number, api_key):
-        
-        try:
-            query = "insert into users (username, password, first_name, last_name, email, phone_number, last_login, api_key) values ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', now(), '{6}');".format(username, password, first_name, last_name, email, phone_number, api_key)
-            # print(query)
-            self.cursor.execute(query)
-            self.db.commit()
-            return "success"
-        except Exception as e:
-            print( e)
-    
-    def update_values(self, apikey, fieldname, deviceID, temp, humidity, moisture, light):
-        try:
-            self.cursor.execute("select api_key from users;")
-            output = self.cursor.fetchall()
-            dummy = []
-            for i in output:
-                dummy.append(i[0])
-            if apikey in dummy:
-                
-                query = 'insert into {0} (deviceID, temperature, humidity, moisture, light, date_time) values("{1}", {2}, {3}, {4}, {5}, now());'.format(fieldname, deviceID, temp, humidity, moisture, light)
-                self.cursor.execute(query)
-                self.db.commit()
-
-                query = 'update Node set temperature={0}, humidity={1}, moisture = {2}, light={3} where deviceID="{4}";'.format(temp, humidity, moisture, light, deviceID)
-                self.cursor.execute(query)
-                self.db.commit()
-
-                return True
-
-            else:
-                print("not available")
-
-        except Exception as e:
-            print("[ERROR!]")
+            print('[error] error executing query!')
             print(e)
